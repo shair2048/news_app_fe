625ee114d152560491953df28002b3f1b8f45d1a
SHair <sonhai.it.2048@gmail.com>
2025-06-11 00:44:15 +0700

    Created app bar for home screen

--- lib/features/home/view/pages/home_page.dart
+++ lib/features/home/view/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:news_app_fe/core/widgets/common_app_bar.dart';
 import 'package:news_app_fe/features/home/view/widgets/most_read.dart';
+import 'package:news_app_fe/features/home/view/widgets/search_app_bar.dart';
 import 'package:news_app_fe/features/home/view/widgets/top_category.dart';
 
 class HomePage extends ConsumerStatefulWidget {
@@ -15,15 +15,16 @@ class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CommonAppBar(appBarTitle: 'Home'),
-      body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            // Top Category
-            TopCategory(),
-            MostRead(),
-          ],
-        ),
+      appBar: SearchAppBar(),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+        children: [
+          Text('TOP CATEGORY'),
+          const SizedBox(height: 16.0),
+          Text('MOST READ'),
+          const SizedBox(height: 16.0),
+          // Add more widgets here as needed
+        ],
       ),
     );
   }
